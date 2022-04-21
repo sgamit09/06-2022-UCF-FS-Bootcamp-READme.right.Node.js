0ddbf7ae8f71ee2345c46d098293f8ed758b1ad6
GigaScott <scott.gamit.09@outlook.com>
2022-04-20 21:39:34 -0400

    updated index.js with prompts

--- index.js
+++ index.js
@@ -1,13 +1,103 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = async () => {
+    const data = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'projAuthor',
+            message: 'Author of Project: ',
+        },
+        {
+            type: 'input',
+            name: 'projTitle',
+            message: 'Title of Project: ',
+        },
+        {
+            type: 'input',
+            name: 'projDescription',
+            message: 'Description of Project: ',
+        },
+        {
+            type: 'checkbox',
+            name: 'projInstallation',
+            message: 'Softwares to Install:  ',
+            choices: ["Google Chrome or Web Browser", "Node.js", "npm modules", "APIs"]
+        },
+        {
+            type: 'input',
+            name: 'npmInstall',
+            message: 'Insert npm commands installed (separate by commas, if none type N/A): ',
+        },
+        {
+            type: 'input',
+            name: 'projUsage',
+            message: 'Usage of Project: ',
+        },
+        {
+            type: 'input',
+            name: 'screenshotURL',
+            message: 'Enter image URL for screenshot of Project: ',
+        },
+        {
+            type: 'list',
+            name: 'projLicense',
+            message: 'List of Licenses your project has:  ',
+            choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None', 'Drivers'],
+        },
+        {
+            type: 'confirm',
+            name: 'projGuidelines',
+            message: 'Add Guidelines for Contribution? (Y/N)',
+        },
+        {
+            type: 'confirm',
+            name: 'projTesting',
+            message: 'Add Testing Section? (Y/N)',
+        },
+        {
+            type: 'input',
+            name: 'projQuestion1',
+            message: 'Enter GitHub Username: ',
+        },
+        {
+            type: 'input',
+            name: 'projQuestion2',
+            message: 'Enter email address: ',
+        },
+        {
+            type: 'choices',
+            name: 'Contributors',
+            message: 'Who contributed to this project?  ',
+            choices: ['Ned', 'Spider-Man', 'Green-Goblin', 'Doc-Ock', 'Multiverse Me']
+        },
+    ])
+        .then((data) => { writeToFile(data) })
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const { projAuthor, projTitle, projDescription, projInstallation, npmInstall, projUsage, screenshotURL,
+        projLicense, projGuidelines, projTesting, projQuestion1, projQuestion2, Contributors
+          } = data;
+
+    console.log(projAuthor);
+    console.log(projTitle);
+    console.log(projDescription); 
+    console.log(projInstallation);   
+
+    // fileName = 'READme.md'
+    // let content = generateMarkdown(data)
+    // fs.watchFile(fileName, content);
+    // console.log("READme Created!")
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions();
+}
 
 // Function call to initialize app
 init();
