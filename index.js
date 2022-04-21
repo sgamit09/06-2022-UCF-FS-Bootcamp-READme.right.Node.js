9c444b30d0eceaca3d98b7f74eb2990b4263549c
GigaScott <scott.gamit.09@outlook.com>
2022-04-21 17:15:16 -0400

    prompts and markdowns are ready testing.

--- index.js
+++ index.js
@@ -5,49 +5,48 @@ const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
 const questions = async () => {
     const data = await inquirer.prompt([
-        // {
-        //     type: 'input',
-        //     name: 'projAuthor',
-        //     message: 'Author of Project: ',
-        // },
-        // {
-        //     type: 'input',
-        //     name: 'projTitle',
-        //     message: 'Title of Project: ',
-        // },
-        // {
-        //     type: 'input',
-        //     name: 'projDescription',
-        //     message: 'Description of Project: ',
-        // },
-        // {
-        //     //object
-        //     type: 'checkbox',
-        //     name: 'projInstallation',
-        //     message: 'Softwares to Install:  ',
-        //     choices: ["Google Chrome or Web Browser", "Node.js", "npm modules", "APIs"]
-        // },
-        // {
-        //     type: 'input',
-        //     name: 'npmInstall',
-        //     message: 'Insert npm commands installed (separate by commas, if none type N/A): ',
-        // },
-        // {
-        //     type: 'input',
-        //     name: 'projUsage',
-        //     message: 'Usage of Project: ',
-        // },
-        // {
-        //     type: 'input',
-        //     name: 'screenshotURL',
-        //     message: 'Enter image URL for screenshot of Project (relative path): ',
-        // },
-        // {
-        //     type: 'list',
-        //     name: 'projLicense',
-        //     message: 'List of Licenses your project has:  ',
-        //     choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None', 'Drivers'],
-        // },
+        {
+            type: 'input',
+            name: 'projAuthor',
+            message: 'Author of Project: ',
+        },
+        {
+            type: 'input',
+            name: 'projTitle',
+            message: 'Title of Project: ',
+        },
+        {
+            type: 'input',
+            name: 'projDescription',
+            message: 'Description of Project: ',
+        },
+        {
+            type: 'checkbox',
+            name: 'projInstallation',
+            message: 'Softwares to Install:  ',
+            choices: ["Google Chrome or Web Browser", "Node.js", "npm modules", "APIs"]
+        },
+        {
+            type: 'input',
+            name: 'npmInstall',
+            message: 'Insert npm commands installed (separate by commas, if none type N/A): ',
+        },
+        {
+            type: 'input',
+            name: 'projUsage',
+            message: 'Usage of Project: ',
+        },
+        {
+            type: 'input',
+            name: 'screenshotURL',
+            message: 'Enter image URL for screenshot of Project (relative path): ',
+        },
+        {
+            type: 'list',
+            name: 'projLicense',
+            message: 'List of Licenses your project has:  ',
+            choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None', 'Drivers'],
+        },
         {
             type: 'confirm',
             name: 'projGuidelines',
@@ -58,27 +57,27 @@ const questions = async () => {
             name: 'projTesting',
             message: 'Add Testing Section? ',
         },
-        // {
-        //     type: 'input',
-        //     name: 'projQuestion1',
-        //     message: 'Enter GitHub Username: ',
-        // },
-        // {
-        //     type: 'input',
-        //     name: 'projQuestion2',
-        //     message: 'Enter GitHub profile link: ',
-        // },
-        // {
-        //     type: 'input',
-        //     name: 'projQuestion3',
-        //     message: 'Enter email address: ',
-        // },
-        // {
-        //     type: 'checkbox',
-        //     name: 'projContributors',
-        //     message: 'Who contributed to this project?  ',
-        //     choices: ['Ned', 'Spider-Man', 'Green-Goblin', 'Doc-Ock', 'Multiverse Me']
-        // },
+        {
+            type: 'input',
+            name: 'projQuestion1',
+            message: 'Enter GitHub Username: ',
+        },
+        {
+            type: 'input',
+            name: 'projQuestion2',
+            message: 'Enter GitHub profile link: ',
+        },
+        {
+            type: 'input',
+            name: 'projQuestion3',
+            message: 'Enter email address: ',
+        },
+        {
+            type: 'checkbox',
+            name: 'projContributors',
+            message: 'Who contributed to this project?  ',
+            choices: ['Ned', 'Spider-Man', 'Green-Goblin', 'Doc-Ock', 'Multiverse Me']
+        },
     ])
         .then((data) => { 
 
@@ -90,17 +89,9 @@ const questions = async () => {
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
 
-//    const { projAuthor, projTitle, projDescription, projInstallation, npmInstall, projUsage, screenshotURL,
-//          projLicense, projGuidelines, projTesting, projQuestion1, projQuestion2, Contributors
-//          } = data;
-
-     generateMarkdown(data);
-
-
-    // fileName = 'READme.md'
-    // let content = generateMarkdown(data)
-    // fs.watchFile(fileName, content);
-    // console.log("READme Created!")
+    let content = generateMarkdown(data);
+    fs.watchFile(fileName, content);
+    console.log("READme Created!")
 }
 
 // TODO: Create a function to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,113 +1,149 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (license === "Apache 2.0") {
+        return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]`
+    } if (license === "MIT license") {
+        return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`
+    } if (license === "ISC License") {
+        return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]`
+    } if (license === "None") {
+        return `None`
+    } if (license === "Drivers") {
+        return `Beep Beep`
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { 
+
+    if (license === "Apache 2.0") {
+        return `(https://opensource.org/licenses/Apache-2.0)`
+    } if (license === "MIT license") {
+        return `(https://opensource.org/licenses/MIT)`
+    } if (license === "ISC License") {
+        return `(https://opensource.org/licenses/ISC)`
+    } if (license === "None") {
+        return `None`
+    } if (license === "Drivers") {
+        return `Beep Beep`
+    }
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    if (license !== "None") {
+        return `## License
+          
+          This project is licensed under ${license}.`
+    } else {
+        return "";
+    }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-// this works!!!
 
-if(data.projTesting === 'Yes' && data.projGuidelines === 'Yes'){
-    let projTesting =  "Insert Project Testing";
-    let projGuidelines = "Insert Project Guidelines";
-    return projGuidelines, projTesting;  
-}
-console.log(projTesting);
-console.log(projGuidelines);
-// } else if (data.projTesting === 'Yes' && data.projGuidelines === 'No'){
-//     let projTesting =  "Insert Project Testing";
-//     let projGuidelines = "N/A";
-//     console.log(projTesting);
-//     console.log(projGuidelines);
-//     return projGuidelines, projTesting; 
-// } else if (data.projTesting === 'No' && data.projGuidelines === 'No') {
-//     let projTesting =  "N/A";
-//     let projGuidelines = "N/A";
-//     console.log(projTesting);
-//     console.log(projGuidelines);
-//     return projGuidelines, projTesting;   
-// }
+    projTestingContent = "1. Fork Code from Github 2. Copy Code onto Local 3. Install any npm 4. Run Code 5. Profit"
+    projGuidelinesContent = "If you have a suggestion that would make this better, please fork the repo and create a pull request. You can also simply open an issue with the tag enhancement. Don't forget to give the project a star! Thanks again!"
 
-// console.log(data.projTitle);
-// console.log(data.projDescription);
-// console.log(data.projInstallation);
+    //If statements to add Project Testing and Guidelines Information   
+    if (data.projTesting === true && data.projGuidelines === true) {
+        let projTesting = projTestingContent;
+        let projGuidelines = projGuidelinesContent;
+        return projGuidelines, projTesting;
+    } else if (data.projTesting === true && data.projGuidelines === false) {
+        let projTesting = projTestingContent
+        let projGuidelines = "N/A";
+        return projGuidelines, projTesting;
+    } else if (data.projTesting === false && data.projGuidelines === true) {
+        let projTesting = "N/A";
+        let projGuidelines = projGuidelinesContent;
+        return projGuidelines, projTesting;
+    } else if (data.projTesting === false && data.projGuidelines === false) {
+        let projTesting = "N/A";
+        let projGuidelines = "N/A";
+        return projGuidelines, projTesting;
+    }
 
-// return `
-// # ${data.projTitle}
-
-// # Table of Content
-
-// -[description](#description)
-// -[installation](#installation)
-// -[usage](#usage)
-// -[licenses](#licenses)
-// -[contribution](#contribution)
-// -[test](#test)
-// -[username](#username)
-// -[profile](#profile)
-
-  
-// ##Description: 
-
-// ${data.projDescription}
-
-    
-// ##Installation:
-
-// Softwares Needed:
-
-// * ${data.projInstallation[0]}
-// * ${data.projInstallation[1]}
-// * ${data.projInstallation[2]}
-// * ${data.projInstallation[3]}
+    let licenseSectionContent = renderLicenseSection(data.license);
+    let licenseSelectionBadge = renderLicenseBadge(data.license);
+    let licenseLinkBadge = renderLicenseBadge(data.license);
 
 
-// ##Usage:
 
-// ${data.projUsage}
+    return `
+    # ${data.projTitle}
 
-// ![Project Screenshot] (${data.screenshotURL})
+    # Table of Content
 
-    
-// ##Licenses:
-
-// ${data.projLicense}
-
-
-// ##Testing:
-
-// ${projTesting}
-
-
-// ##Contribution:
-
-// ${projGuidelines}
+    -[description](#description)
+    -[installation](#installation)
+    -[usage](#usage)
+    -[licenses](#licenses)
+    -[contribution](#contribution)
+    -[test](#test)
+    -[username](#username)
+    -[profile](#profile)
 
 
-// ##Questions:
-// For any questions,
+    ##Description: 
 
-// * ${data.projAuthor} -  ${data.projQuestion3}
+    ${data.projDescription}
 
 
-// ##GitHub profile:
-// * ${data.projQuestion1} - ${data.projQuestion2}
+    ##Installation:
 
-// ## Contributors:
+    Softwares Needed:
 
-// * ${data.projContributors[0]}
-// * ${data.projContributors[1]}
-// * ${data.projContributors[2]}
-// * ${data.projContributors[3]}
-// * ${data.projContributors[4]}
-// `;
+    * ${data.projInstallation[0]}
+    * ${data.projInstallation[1]}
+    * ${data.projInstallation[2]}
+    * ${data.projInstallation[3]}
+
+
+    ##Usage:
+
+    ${data.projUsage}
+
+    ![Project Screenshot] (${data.screenshotURL})
+
+
+    ${licenseSectionContent}
+
+    ${licenseSelectionBadge}${licenseLinkBadge}
+
+
+    ##Testing:
+
+    ${projTesting}
+
+
+    ##Contribution:
+
+    ${projGuidelines}
+
+
+    ##Questions:
+    For any questions,
+
+    * ${data.projAuthor} -  ${data.projQuestion3}
+
+
+    ##GitHub profile:
+    * ${data.projQuestion1} - ${data.projQuestion2}
+
+    ## Contributors:
+
+    * ${data.projContributors[0]}
+    * ${data.projContributors[1]}
+    * ${data.projContributors[2]}
+    * ${data.projContributors[3]}
+    * ${data.projContributors[4]}
+    `;
 }
 
 
